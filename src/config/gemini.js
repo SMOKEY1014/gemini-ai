712e066aa91e260b04c356a5e7768930663cf8c7
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-09-02 16:06:57 +0200

    first commit

---
+++ src/config/gemini.js
@@ -0,0 +1,43 @@
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ */
+// Load environment variables from the .env file
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+// const apiKey2 = process.env.REACT_APP_GEMINI_API_KEY;
+const apiKey = "AIzaSyDJ52JFUt-Ki99X3dO7VTxLFGW2vHZpVfA";
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    // safetySettings: Adjust safety settings
+    // See https://ai.google.dev/gemini-api/docs/safety-settings
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+  return result.response.text();
+}
+
+export default run;
